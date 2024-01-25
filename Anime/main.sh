fe3cf3c5cd73a1dfe2fb43c7897eb7021e0a2a5a
THEDESTROYER6667 <131635573+THEDESTROYER6667@users.noreply.github.com>
2024-01-25 11:20:45 -0500

    Update main.sh

--- Anime/main.sh
+++ Anime/main.sh
@@ -2,7 +2,7 @@ main() {
   echo -e "Changing directory...."
   cd ~/Downloads/
   echo -e "Getting theme...."
-  curl "https://github.com/THEDESTROYER6667/MacSploit-Themes/blob/main/Anime/tomorrow-theme_night.js" -o "./theme-tomorrow_night.js"
+  curl "https://raw.githubusercontent.com/THEDESTROYER6667/MacSploit-Themes/main/Anime/theme-tomorrow_night.js" -o "./theme-tomorrow_night.js"
   echo -e "Applying theme...."
   mv ~/Downloads/theme-tomorrow_night.js /Applications/Macsploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle/theme-tomorrow_night.js
   echo -e "If your executor is open, make sure to quit and reopen it."
