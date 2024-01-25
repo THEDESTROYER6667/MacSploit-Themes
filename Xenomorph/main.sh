878d749af37b8d2a4cf0f54313cf27674edcee9b
THEDESTROYER6667 <131635573+THEDESTROYER6667@users.noreply.github.com>
2024-01-25 09:51:35 -0500

    Update main.sh

--- Xenomorph/main.sh
+++ Xenomorph/main.sh
@@ -1,1 +1,10 @@
-
+main() {
+  echo -e "Changing directory...."
+  cd ~/Downloads/
+  echo -e "Getting theme...."
+  curl "https://raw.githubusercontent.com/THEDESTROYER6667/MacSploit-Themes/main/theme-tomorrow_night.js" -o "./theme-tomorrow_night.js"
+  echo -e "Applying theme...."
+  mv ~/Downloads/theme-tomorrow_night.js /Applications/Macsploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle/theme-tomorrow_night.js
+  echo -e "If your executor is open, make sure to quit and reopen it."
+  echo -e "Enjoy your xenomorph theme!"
+}
