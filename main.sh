b1d42d94f9cdd1d26da91bbe5bd9310b5517495e
THEDESTROYER6667 <131635573+THEDESTROYER6667@users.noreply.github.com>
2024-01-24 20:02:35 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,10 +1,8 @@
 main() {
   echo -e "Changing directory..."
-  cd ~/Applications/Macsploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle/theme-tomorrow_night.js -o "./theme-tomorrow_night.js"
-  echo -e "Done!"
+  cd ~/Applications/Macsploit.app/Contents/Resources/CodeViewer_CodeViewer.bundle/Contents/Resources/ace.bundle/theme-tomorrow_night.js
   echo -e "Getting theme..."
-  curl "https://raw.githubusercontent.com/THEDESTROYER6667/MacSploit-Themes/main/theme-tomorrow_night.js"
-  echo -e "Done!"
+  curl "https://raw.githubusercontent.com/THEDESTROYER6667/MacSploit-Themes/main/theme-tomorrow_night.js" -o "./theme-tomorrow_night.js"
   echo -e "If your executor is open, make sure to quit and reopen it."
   echo -e "Enjoy!"
 }
